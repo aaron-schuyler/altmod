f324174c579c3e8869520c931b705fb19a54204e
aaron-schuyler <aaron@yeahthatcontent.com>
2020-05-12 16:46:09 -0400

    testing

--- client/altmod/src/axios.js
+++ client/altmod/src/axios.js
@@ -4,6 +4,6 @@ axios.defaults.withCredentials = true; // enable axios post cookie, default fals
 export default () => {
     return axios.create({
         //baseURL: `http://192.168.1.92:8000`
-        baseURL: `ec2-3-15-160-9.us-east-2.compute.amazonaws.com:8000`
+        baseURL: `http://ec2-3-15-160-9.us-east-2.compute.amazonaws.com:8000`
     });
 };
